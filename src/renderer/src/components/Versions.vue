7a3afb34a78d69c7a318a4662b440aedfde52a59
bobingo <17642059382@163.com>
2022-10-20 18:32:12 +0800

    创建electron-vite项目模版

---
+++ src/renderer/src/components/Versions.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+
+const versions = reactive({ ...window.electron.process.versions })
+const getEnv = (): void => {
+  console.log('window.electron.process', window.electron, window.api)
+}
+
+
+window.electron.ipcRenderer.on('configfilePaths', (_, message) => {
+      console.log('选择的配置文件----', message);
+    });
+
+
+</script>
+
+<template>
+  <ul class="versions">
+    <li class="electron-version" @click="getEnv">Electron v{{ versions.electron }}</li>
+    <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
+    <li class="node-version">Node v{{ versions.node }}</li>
+    <li class="v8-version">V8 v{{ versions.v8 }}</li>
+  </ul>
+</template>
