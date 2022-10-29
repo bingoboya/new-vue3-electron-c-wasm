aae45e930635a63813194346573ec7c105e2a657
bobingo <17642059382@163.com>
2022-10-29 23:58:10 +0800

    增加HandleExe.vue文件

--- src/renderer/src/views/DashBoard/HandleExe.vue
+++ src/renderer/src/views/DashBoard/HandleExe.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <button @click="checkoutRootDir">查看根目录1</button>
+      <button @click="checkoutRootDir">查看根目录1</button>
       <button @click="startExe">启动exe</button>
       <button @click="communicationExe">与exe通信</button>
       <button @click="killExe">关闭exe</button>
     </div>
 </template>
 <script setup>
+/*
+  该文件内的操作直接在 DashBoard/index.vue中执行时会出现无效的情况（仅出现在执行脚本 [yarn build:win] 打包windows环境的生产包时），原因未知
+  解决：新建HandleExe.vue文件，将代码移动到该文件中，再在DashBoard/index.vue中引用，就可以解决
+*/
 const isInElectron = navigator.userAgent.toLowerCase().indexOf(' electron/') > -1
 const runonRightEnv = isInElectron && navigator.platform === 'Win32'
 console.log('运行在electron环境:', isInElectron, 'navigator.platform1', navigator.platform)
